1ef5f9ebabab83396ac5d6298096ac4759e0203c
nathan-applied <nathan@appliedimagination.com>
2025-05-04 11:15:56 -0700

    change slack messaging to not use blocks

--- slack.js
+++ slack.js
@@ -2,24 +2,17 @@
 const sendSlackNotification = async (jobs) => {
     const webhookUrl = process.env.SLACK_WEBHOOK_URL; // Best practice: store in env var
 
-    let blocks = []
-    for(let k of Object.keys(jobs)){
-        let job_links = [];
-        for (let jobj of jobs[k]) {
-            job_links.push(`<${jobj.link}|${jobj.title}>*\n`)
+    let messageLines = ['*🆕 New jobs posted!*'];
+
+    for (let company of Object.keys(jobs)) {
+        messageLines.push(`\r\n*${company}*`);
+        for (let job of jobs[company]) {
+            messageLines.push(`<${job.link}|${job.title}>`);
         }
-        blocks.push({
-            type: "section",
-            text: {
-                type: "mrkdwn",
-                text: `*New Jobs From ${k}*: \r\n${job_links.join('\r\n')}`
-            }
-        });
     }
 
     const payload = {
-        text: `🆕 New jobs posted!`,
-        blocks
+        text: messageLines.join('\n')
     };
 
     console.log(payload)
